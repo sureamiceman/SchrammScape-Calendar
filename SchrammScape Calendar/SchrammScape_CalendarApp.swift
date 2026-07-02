488bfbdd987c13b98ff7a99cd744bc3d0bf6f919
Tim Schramm <tschramm2010@gmail.com>
2026-07-01 21:51:36 -0400

    Initial Commit

---
+++ SchrammScape Calendar/SchrammScape_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrammScape_CalendarApp.swift
+//  SchrammScape Calendar
+//
+//  Created by Tim Schramm on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SchrammScape_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
